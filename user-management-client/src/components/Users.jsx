3e37908f9073ddf5fc1050c34ec17397c51b048e
SK-Washif <washiffishal@gmail.com>
2025-10-20 00:55:35 +0600

    create POST method and data now update client site and server site both, ( use cors, express.json() , useState method)

--- user-management-client/src/components/Users.jsx
+++ user-management-client/src/components/Users.jsx
@@ -1,9 +1,11 @@
-import React, { use } from 'react'
+import React, { use, useState } from 'react'
 
 
 
 const Users = ({userPromise}) => {
-  const users =use(userPromise)
+  const initialUsers =use(userPromise);
+  const [users, setUsers] = useState(initialUsers);
+  console.log(users);
 
   const handleAddUsers = e =>{
   e.preventDefault();
@@ -11,6 +13,21 @@ const Users = ({userPromise}) => {
   const email = e.target.email.value;
   const user = {name, email}
   console.log(user);
+
+  fetch('http://localhost:9000/users',{
+    method: 'POST',
+    headers:{
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res => res.json())
+  .then(data =>{
+    console.log("Data after post:", data);
+    const newUsers = [...users,data]
+    setUsers(newUsers);
+    e.target.reset();
+  })
 }
 
   return (
--- user-management-server/index.js
+++ user-management-server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(cors());
+app.use(express.json());
 
 const users =[
   {id:1, name:'washif', email:'washif@gmail.com'},
@@ -13,6 +14,14 @@ const users =[
 app.get('/users',(req, res)=>{
   res.send(users);
 });
+
+app.post('/users', (req, res) =>{
+  console.log(req.body);
+  const newUser = req.body
+  newUser.id = users.length+1;
+  users.push(newUser);
+  res.send(newUser);
+})
 
 app.listen(port, (req, res)=>{
   console.log(`The port number is: ${port}`);
