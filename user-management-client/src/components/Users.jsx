21bf9d0fe8b28b80542845058aee3e066122ad52
SK-Washif <washiffishal@gmail.com>
2025-10-20 00:32:03 +0600

    create a input field for take dara , dara just store in )console(

--- user-management-client/src/components/Users.jsx
+++ user-management-client/src/components/Users.jsx
@@ -1,13 +1,37 @@
 import React, { use } from 'react'
 
+
+
 const Users = ({userPromise}) => {
   const users =use(userPromise)
+
+  const handleAddUsers = e =>{
+  e.preventDefault();
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const user = {name, email}
+  console.log(user);
+}
+
   return (
     <div>
 
-      {
+      <div>
+        <form onSubmit={handleAddUsers}>
+          <input name='name' type="text" />
+          <br />
+          <input name='email' type="text" />
+          <br />
+          <input type="submit" value='Add User' />
+          <br />
+        </form>
+      </div>
+
+      <div>
+        {
       users.map(user => <li key={user.id}>{user.name} : {user.email}</li>)        
       }
+      </div>
     </div>
   )
 }
